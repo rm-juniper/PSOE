adcb20fa4a661f0b359fe4069beab66780014f9c
rm-juniper <kelsey.honkoop@gmail.com>
2021-09-06 17:35:33 +0930

    Fixes and Background

--- sc/configs.sh
+++ sc/configs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo mkdir /usr/share/backgrounds
+sudo cp ../img/default_linux.png /usr/share/backgrounds/
 #Install Configs
 cp -rf ../cfg/dunst ~/.config/
 cp -rf ../cfg/picom.conf ~/.config/
@@ -16,4 +17,3 @@ cd st && sudo make clean install && rm -f config.h && cd ..
 cd slock && sudo make clean install && rm -f config.h && cd ..
 cd xmenu && sudo make clean install && cd ..
 cd ly && sudo make clean install && sudo systemctl enable ly && cd ../..
-sudo cp /img/default_linux.png /usr/share/backgrounds/
